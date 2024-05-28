daf6affa227e1843185a89e27246583f01baf0b5
Mark Tabatskyi <vasenka688@gmail.com>
2024-05-28 20:41:16 +0300

    added opencv, only reflect is working atm

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from matplotlib import pyplot as plt
 def show_matrices(matrices: list, labels: list):
     fig = plt.figure()
     max_value = np.max(matrices)
+    min_value = np.min(matrices)
 
     if all(matrix.shape[1] == 3 for matrix in matrices):
         axis = fig.add_subplot(111, projection='3d')
@@ -14,16 +15,21 @@ def show_matrices(matrices: list, labels: list):
             xs, ys, zs = matrix[:, 0], matrix[:, 1], matrix[:, 2]
             axis.plot(xs, ys, zs, label=labels[i])
 
-        axis.set_xlim(-max_value, max_value)
-        axis.set_ylim(-max_value, max_value)
-        axis.set_zlim(-max_value, max_value)
+        axis.set_xlabel('X')
+        axis.set_ylabel('Y')
+        axis.set_zlabel('Z')
+        axis.set_xlim(min_value, max_value)
+        axis.set_ylim(min_value, max_value)
+        axis.set_zlim(min_value, max_value)
     elif all(matrix.shape[1] == 2 for matrix in matrices):
         axis = fig.add_subplot(111)
         for i, matrix in enumerate(matrices):
             xs, ys = matrix[:, 0], matrix[:, 1]
             axis.plot(xs, ys, label=labels[i])
-        plt.xlim(-max_value, max_value)
-        plt.ylim(-max_value, max_value)
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.xlim(min_value, max_value)
+        plt.ylim(min_value, max_value)
     else:
         raise ValueError("Impossible to show this matrices.")
 
@@ -43,6 +49,15 @@ def transform_by_matrix(original_matrix: np.array, transform_matrix: np.array, o
     show_matrices([original_matrix, transformed_matrix], ['Original', operation_label])
 
 
+def ensure_2d_vectors(matrix):
+    matrix = np.squeeze(matrix)
+
+    if matrix.ndim > 2:
+        matrix = matrix.reshape(-1, matrix.shape[-1])
+
+    return matrix
+
+
 def scale_matrix(original_matrix: np.array, scales: list):
     trans_matrix = np.diag(scales)
     transform_by_matrix(original_matrix, trans_matrix, f"Scaled by {scales}")
@@ -53,10 +68,7 @@ def rotate_matrix(original_matrix: np.array, angle: float, axis='y'):
     cos_a, sin_a = math.cos(angle_rad), math.sin(angle_rad)
 
     if original_matrix.shape[1] == 2:
-        trans_matrix = np.array([
-            [cos_a, -sin_a],
-            [sin_a, cos_a]
-        ])
+        trans_matrix = np.array([[cos_a, -sin_a], [sin_a, cos_a]])
     elif original_matrix.shape[1] == 3:
         match axis:
             case 'x':
@@ -86,10 +98,47 @@ def angle_matrix(original_matrix: np.array, k: float, fixed_axis: int, variable_
     transform_by_matrix(original_matrix, trans_matrix, f"Angled by {k} along {fixed_axis} axis")
 
 
-batman = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1, 0.2], [0, 0]])
+def rotate_image(image, angle):
+    (height, width) = image.shape[:2]
+    center = (width // 2, height // 2)
+    matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, matrix, (width, height))
+    rotated = ensure_2d_vectors(rotated)
+    show_matrices([image, rotated], ["Original", f"Rotated by {angle}º (OpenCV)"])
+
+
+# def scale_image(image, scale_x, scale_y):
+#     scaled = cv2.resize(image, None, fx=scale_x, fy=scale_y)
+#     print(type(scaled))
+#     print(scaled.shape)
+#     print(scaled)
+#     scaled = ensure_2d_vectors(scaled)
+#     print(scaled)
+#     show_matrices([image, scaled], ["Original", f"Scaled by {scale_x}, {scale_y} (OpenCV)"])
+
+
+def reflect_image(image, axis):
+    if axis == 'x':
+        reflected = cv2.flip(image, 0)
+    else:
+        reflected = cv2.flip(image, 1)
+    show_matrices([image, reflected], ["Original", f"Reflected across {axis} (OpenCV)"])
+
+
+def angle_image(image, k, axis):
+    (height, width) = image.shape[:2]
+    if axis == 'x':
+        matrix = np.float32([[1, k, 0], [0, 1, 0]])
+    else:
+        matrix = np.float32([[1, 0, 0], [k, 1, 0]])
+    angled = cv2.warpAffine(image, matrix, (width, height))
+    show_matrices([image, angled], ["Original", f"Angled by {k} along {axis} axis (OpenCV)"])
+
+
+batman = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1.5, 0.5], [0, 0]])
 rotate_matrix(batman, 45)
 scale_matrix(batman, [2, 2])
-reflect_matrix(batman, [False, True])
+reflect_matrix(batman, [True, True])
 angle_matrix(batman, 2, 0, 1)
 
 trans_matrix = np.array([[0, 1], [1, 0]])
@@ -98,9 +147,14 @@ transform_by_matrix(batman, trans_matrix)
 cube = np.array([[1, 0, 0], [1, 1, 0], [0, 1, 0], [0, 0, 0], [1, 0, 1], [1, 1, 1], [0, 1, 1], [0, 0, 1]])
 show_matrices([cube], ['Original'])
 
-rotate_matrix(cube, 45, (0, 1))
-rotate_matrix(cube, 45, (1, 2))
-rotate_matrix(cube, 45, (0, 2))
+rotate_matrix(cube, 45, 'x')
+rotate_matrix(cube, 45, 'y')
+rotate_matrix(cube, 45, 'z')
 scale_matrix(cube, [1, 2, 1])
-reflect_matrix(cube, [True, False, False])
+reflect_matrix(cube, [True, True, False])
 angle_matrix(cube, 1, 2, 1)
+
+rotate_image(batman, 45)
+# scale_image(batman, 2, 2)
+reflect_image(batman, 'y')
+angle_image(batman, 0.5, 'x')
