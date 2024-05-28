2b8a87e54040f49183b3b88b57c729bf99f59e02
Mark Tabatskyi <vasenka688@gmail.com>
2024-05-28 11:15:36 +0300

    trying in 3d

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def show_transformation(matrices: list, labels: list):
-    axis = plt.subplots()[1]
-    colors = ['b', 'r', 'g', 'c', 'm', 'y', 'k']
+def show_transformation(matrices: list, labels: list, dimensions=3):
+    fig = plt.figure()
+    if dimensions == 3:
+        axis = fig.add_subplot(111, projection='3d')
+        for i, matrix in enumerate(matrices):
+            if matrix.shape[1] < 3:
+                raise ValueError("Matrix must have at least three dimensions for 3D plotting")
+            xs, ys, zs = matrix[:, 0], matrix[:, 1], matrix[:, 2]
+            axis.plot(xs, ys, zs, label=labels[i])
 
-    i = 0
-    for matrix in matrices:
-        axis.add_patch(plt.Polygon(matrix, closed=True, fill=None, edgecolor=colors[i], label=labels[i]))
-        i += 1
-
-    max_value = np.max(np.abs(matrices))
-    plt.xlim(-max_value, max_value)
-    plt.ylim(-max_value, max_value)
+        axis.set_xlabel('X')
+        axis.set_ylabel('Y')
+        axis.set_zlabel('Z')
+    else:
+        axis = fig.add_subplot(111)
+        for i, matrix in enumerate(matrices):
+            xs, ys = matrix[:, 0], matrix[:, 1]
+            axis.plot(xs, ys, label=labels[i])
 
     plt.grid(True)
     plt.legend()
@@ -32,38 +38,44 @@ def transform_by_matrix(original_matrix: np.array, transform_matrix: np.array, o
     show_transformation([original_matrix, transformed_matrix], ['Original', operation_label])
 
 
-def rotate_matrix(original_matrix: np.array, angle: float):
+def scale_matrix(original_matrix: np.array, scales: list):
+    trans_matrix = np.diag(scales)
+    transform_by_matrix(original_matrix, trans_matrix, f"Scaled by {scales}")
+
+
+def rotate_matrix(original_matrix: np.array, angle: float, plane=(0, 1)):
     angle_rad = math.radians(angle)
-    trans_matrix = np.array([[math.cos(angle_rad), -math.sin(angle_rad)], [math.sin(angle_rad), math.cos(angle_rad)]])
-    transform_by_matrix(original_matrix, trans_matrix, f"Rotated by {angle}º")
+    cos_a, sin_a = math.cos(angle_rad), math.sin(angle_rad)
+    trans_matrix = np.eye(original_matrix.shape[1])
+    x, y = plane
+    trans_matrix[[x, x, y, y], [x, y, x, y]] = [cos_a, -sin_a, sin_a, cos_a]
+    transform_by_matrix(original_matrix, trans_matrix, f"Rotated by {angle}º in {plane} plane")
 
 
-def scale_matrix(original_matrix: np.array, scale_x: float, scale_y: float):
-    trans_matrix = np.array([[scale_x, 0], [0, scale_y]])
-    transform_by_matrix(original_matrix, trans_matrix, f"Scaled by {scale_x} on x and {scale_y} on y")
+def reflect_matrix(original_matrix: np.array, axes: list):
+    reflect_vector = [-1 if axis else 1 for axis in axes]
+    trans_matrix = np.diag(reflect_vector)
+    transform_by_matrix(original_matrix, trans_matrix, f"Reflected across {axes}")
 
 
-def reflect_matrix(original_matrix: np.array, axis: str):
-    if axis == 'x':
-        trans_matrix = np.array([[1, 0], [0, -1]])
-    elif axis == 'y':
-        trans_matrix = np.array([[-1, 0], [0, 1]])
-    transform_by_matrix(original_matrix, trans_matrix, f"Reflected by {axis}")
+def angle_matrix(original_matrix: np.array, k: float, fixed_axis: int, variable_axis: int):
+    trans_matrix = np.eye(original_matrix.shape[1])
+    trans_matrix[variable_axis, fixed_axis] = k
+    transform_by_matrix(original_matrix, trans_matrix, f"Angled by {k} along {fixed_axis} axis")
 
 
-def angle_matrix(original_matrix: np.array, k: float, axis: str):
-    if axis == 'x':
-        trans_matrix = np.array([[1, k], [0, 1]])
-    else:
-        trans_matrix = np.array([[1, 0], [k, 1]])
-    transform_by_matrix(original_matrix, trans_matrix, f"Angled by {axis} with {k} coefficient")
+# batman = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1, 0.2], [0, 0]])
+# rotate_matrix(batman, 45)
+# scale_matrix(batman, [2, 2])
+# reflect_matrix(batman, [True, False])
+# angle_matrix(batman, 2, 0, 1)
+#
+# trans_matrix = np.array([[0, 1], [1, 0]])
+# transform_by_matrix(batman, trans_matrix)
 
+cube = np.array([[1, 0, 0], [1, 1, 0], [0, 1, 0], [0, 0, 0], [1, 0, 1], [1, 1, 1], [0, 1, 1], [0, 0, 1]])
 
-batman = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1, 0.2], [0, 0]])
-rotate_matrix(batman, 45)
-scale_matrix(batman, 2, 2)
-reflect_matrix(batman, 'x')
-angle_matrix(batman, 2, 'y')
-
-trans_matrix = np.array([[0, 1], [1, 0]])
-transform_by_matrix(batman, trans_matrix)
+rotate_matrix(cube, 45, (0, 1))
+scale_matrix(cube, [1, 2, 1])
+reflect_matrix(cube, [True, False, False])
+angle_matrix(cube, 1, 2, 1)
