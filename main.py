aec4e38d29123d6ef12127d3d4e757daa303d6e4
Mark Tabatskyi <vasenka688@gmail.com>
2024-05-29 18:14:42 +0300

    working but i dont like it

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def show_matrices(matrices: list, labels: list):
+def show_matrices(matrices: list, labels: list, save=False):
     fig = plt.figure()
     max_value = np.max(matrices)
     min_value = np.min(matrices)
@@ -33,8 +33,11 @@ def show_matrices(matrices: list, labels: list):
     else:
         raise ValueError("Impossible to show this matrices.")
 
-    plt.grid(True)
-    plt.legend()
+    if not save:
+        plt.grid(True)
+        plt.legend()
+    else:
+        plt.savefig('plot.png')
     plt.show()
 
 
@@ -47,15 +50,6 @@ def transform_by_matrix(original_matrix: np.array, transform_matrix: np.array, o
         raise ValueError("Impossible to multiply this matrices.")
 
     show_matrices([original_matrix, transformed_matrix], ['Original', operation_label])
-
-
-def ensure_2d_vectors(matrix):
-    matrix = np.squeeze(matrix)
-
-    if matrix.ndim > 2:
-        matrix = matrix.reshape(-1, matrix.shape[-1])
-
-    return matrix
 
 
 def scale_matrix(original_matrix: np.array, scales: list):
@@ -98,23 +92,22 @@ def angle_matrix(original_matrix: np.array, k: float, fixed_axis: int, variable_
     transform_by_matrix(original_matrix, trans_matrix, f"Angled by {k} along {fixed_axis} axis")
 
 
+def show_image(image, message):
+    plt.imshow(image, cmap='gray')
+    plt.show()
+
+
 def rotate_image(image, angle):
     (height, width) = image.shape[:2]
     center = (width // 2, height // 2)
     matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
     rotated = cv2.warpAffine(image, matrix, (width, height))
-    rotated = ensure_2d_vectors(rotated)
-    show_matrices([image, rotated], ["Original", f"Rotated by {angle}º (OpenCV)"])
+    show_image(rotated, f"Rotated by {angle}º")
 
 
-# def scale_image(image, scale_x, scale_y):
-#     scaled = cv2.resize(image, None, fx=scale_x, fy=scale_y)
-#     print(type(scaled))
-#     print(scaled.shape)
-#     print(scaled)
-#     scaled = ensure_2d_vectors(scaled)
-#     print(scaled)
-#     show_matrices([image, scaled], ["Original", f"Scaled by {scale_x}, {scale_y} (OpenCV)"])
+def scale_image(image, scale_x, scale_y):
+    scaled = cv2.resize(image, None, fx=scale_x, fy=scale_y)
+    show_image(scaled, f"Scaled by {scale_x}, {scale_y} (OpenCV)")
 
 
 def reflect_image(image, axis):
@@ -122,7 +115,7 @@ def reflect_image(image, axis):
         reflected = cv2.flip(image, 0)
     else:
         reflected = cv2.flip(image, 1)
-    show_matrices([image, reflected], ["Original", f"Reflected across {axis} (OpenCV)"])
+    show_image(reflected, f"Reflected across {axis} (OpenCV)")
 
 
 def angle_image(image, k, axis):
@@ -132,20 +125,19 @@ def angle_image(image, k, axis):
     else:
         matrix = np.float32([[1, 0, 0], [k, 1, 0]])
     angled = cv2.warpAffine(image, matrix, (width, height))
-    show_matrices([image, angled], ["Original", f"Angled by {k} along {axis} axis (OpenCV)"])
+    show_image(angled, f"Angled by {k} along {axis} axis (OpenCV)")
 
 
 batman = np.array([[0, 0], [1, 0.2], [0.4, 1], [0.5, 0.4], [0, 0.8], [-0.5, 0.4], [-0.4, 1], [-1.5, 0.5], [0, 0]])
 rotate_matrix(batman, 45)
 scale_matrix(batman, [2, 2])
-reflect_matrix(batman, [True, True])
+reflect_matrix(batman, [False, True])
 angle_matrix(batman, 2, 0, 1)
 
 trans_matrix = np.array([[0, 1], [1, 0]])
-transform_by_matrix(batman, trans_matrix)
+transform_by_matrix(batman, trans_matrix, f"Transformed by {trans_matrix} matrix")
 
 cube = np.array([[1, 0, 0], [1, 1, 0], [0, 1, 0], [0, 0, 0], [1, 0, 1], [1, 1, 1], [0, 1, 1], [0, 0, 1]])
-show_matrices([cube], ['Original'])
 
 rotate_matrix(cube, 45, 'x')
 rotate_matrix(cube, 45, 'y')
@@ -154,7 +146,9 @@ scale_matrix(cube, [1, 2, 1])
 reflect_matrix(cube, [True, True, False])
 angle_matrix(cube, 1, 2, 1)
 
-rotate_image(batman, 45)
-# scale_image(batman, 2, 2)
-reflect_image(batman, 'y')
-angle_image(batman, 0.5, 'x')
+show_matrices([batman], [''], True)
+plot = cv2.imread('plot.png')
+rotate_image(plot, 45)
+scale_image(plot, 2, 2)
+reflect_image(plot, 'y')
+angle_image(plot, 0.5, 'x')
